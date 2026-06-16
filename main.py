69d8ebe593260a89bb31767263754092c845c4af
HaviarA1 <haviar.antonin@gmail.com>
2026-06-16 14:52:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from logger import logger
 from errors import ConnectionError
 from djitellopy import Tello, TelloException
-
+import time
+from keyboard import is_pressed
+import cv2
+import turtle
 
 def main(tello: Tello):
 
@@ -12,22 +15,58 @@ def main(tello: Tello):
     logger.success("Connected to Tello")
     logger.info(f"Battery: {tello.get_battery()}%")
     
+    tello.set_speed(40)
     tello.streamon()
+    time.sleep(2)
+    frame_read = tello.get_frame_read()
+    #tello.set_video_resolution(Tello.RESOLUTION_480P)
+    tello.set_video_fps(Tello.FPS_5)
     
-    frame_reader = tello.get_frame_read()
-
-    print(frame_reader.frame)
 
 
-
+    while not is_pressed('space'):
+        pass
+    # takeoff_and_landing(tello)
+    while not is_pressed('esc'):
+        img = frame_read.frame
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        cv2.imshow("Tello Live Stream", img_rgb)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        # while is_pressed('left'):
+        #     tello.rotate_counter_clockwise(45)
+        # while is_pressed('right'):
+        #     tello.rotate_clockwise(45)
+        # while is_pressed('up'):
+        #     tello.move_forward(70)
+        # while is_pressed('down'):
+        #     tello.move_back(70)
+        # while is_pressed('space'):
+        #     tello.move_up(50)
+        # while is_pressed('shift'):
+        #     tello.move_down(50) 
 
 
 def handle_program_exit(tello: Tello):
     # TODO: Is this enough to PROPERLY stop?
 
+    
     logger.info("Exiting program...")
     tello.end()
+    cv2.destroyAllWindows()
     logger.success("Successfuly exited program")
+
+
+
+
+
+
+def takeoff_and_landing(tello: Tello):
+    tello.takeoff()
+    #time.sleep(10)
+    # tello.land()
+    #tello.end()
 
 
 if __name__ == "__main__":
@@ -47,6 +86,5 @@ if __name__ == "__main__":
         logger.fatal("Faild to connect to Tello - \033[91mSTOPPING\033[0m")
     
     except Exception as e:
-        logger.error("Unknow error:", e)
-    
+        logger.error("Unknown error:", e)
     
